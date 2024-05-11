a50ea818675df5e3a52a1d6ec549790afb9ffa01
dk_mac <pivottocoding@gmail.com>
2024-05-11 14:49:13 +0900

    [Feat] video 컨트롤러 생성

---
+++ backend/src/main/java/dk/shorts/video/VideoController.java
@@ -0,0 +1,12 @@
+package dk.shorts.video;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/video")
+@RequiredArgsConstructor
+public class VideoController {
+}
